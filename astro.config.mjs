860e8256dea81ddbb07163c577d1e38db1cda20d
Flávio Roberto Troszczanczuk <flavioczuk@gmail.com>
2024-03-20 16:53:17 -0300

    micro adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,11 @@
 import { defineConfig } from 'astro/config'
 import { fileURLToPath } from 'url'
 import path, { dirname } from 'path'
-import icon from 'astro-icon'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export default defineConfig({
-    integrations: [
-        icon({
-            svgoOptions: {
-                plugins: [{
-                    name: 'preset-default',
-                    params: {
-                        overrides: {
-                            mergePaths: false
-                        }
-                    }
-                }]
-            }
-        })
-    ],
     vite: {
         resolve: {
             alias: {
