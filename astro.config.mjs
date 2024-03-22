595d232dd09af1bc47445495a06773520d4c3753
Flávio Roberto Troszczanczuk <flavioczuk@gmail.com>
2024-03-22 11:34:53 -0300

    fix filepath

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,7 @@
 import { defineConfig } from 'astro/config'
-import { fileURLToPath } from 'url'
-import path, { dirname } from 'path'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 export default defineConfig({
     vite: {
-        resolve: {
-            alias: {
-                '@/': `${path.resolve(__dirname, 'src')}/`
-            }
-        },
         css: {
             preprocessorOptions: {
                 scss: {
