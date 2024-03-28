a0b2ef71d0dd56c4c1e8c00f0e0880c39775bc19
Flávio Roberto Troszczanczuk <flavioczuk@gmail.com>
2024-03-28 16:05:12 -0300

    add copyright

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 // libraries
 import { RecoilRoot } from 'recoil'
+import { useEffect } from 'react'
 
 // components
 import SmoothScrolling from '@/components/utils/smooth-scrolling'
@@ -19,6 +20,12 @@ const antarctica = localFont({
 })
 
 export default function App({ Component, pageProps, router }) {
+
+	useEffect(() => {
+		const message = 'Design Gabriel Leon 🔗 www.behance.net/leonngabr \nCode Senz Design 🔗 www.senzdsn.com'
+		const style = 'color: #f8f8f8; font-size: 12px; font-weight: bold; background-color: #0d0e13; padding: 8px'
+		console.log(`%c${message}`, style)
+	}, [])
 
 	return (
 		<div className={antarctica.className}>
