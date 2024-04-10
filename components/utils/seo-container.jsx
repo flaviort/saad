4f3551a78c0cfc09c06d2e1df0bba24f1b1035e7
Flávio Roberto Troszczanczuk <flavioczuk@gmail.com>
2024-04-10 16:04:02 -0300

    og image

--- components/utils/seo-container.jsx
+++ components/utils/seo-container.jsx
@@ -53,20 +53,18 @@ export default function SEOContainer({ pageTitle, pageDescription, bodyClass }) 
             {/* facebook */}
             <meta property='og:locale' content='en-US' />
             <meta property='og:type' content='website' />
-            <meta property='og:image:width' content='1200' />
-            <meta property='og:image:height' content='630' />
             <meta property='og:title' content={siteName + ' | ' + (pageTitle ?? defaultPageTitle)} />
             <meta property='og:description' content={(pageDescription ?? defaultPageDesc)} />
             <meta property='og:url' content={urlAddress} />
             <meta property='og:site_name' content={siteName} />
-            <meta property='og:image:secure_url' content={image} />
+            <meta property='og:image' content={image} />
 
             {/* twitter */}
             <meta name='twitter:title' content={siteName + ' | ' + (pageTitle ?? defaultPageTitle)} />
             <meta name='twitter:description' content={(pageDescription ?? defaultPageDesc)} />
             <meta name='twitter:url' content={urlAddress} />
             <meta name='twitter:image' content={image} />
-            <meta name='twitter:card' content='summary' />
+            <meta name="twitter:card" content="summary_large_image" />
 
         </Head>
     )
